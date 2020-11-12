4161217377ba3963643ab77632fb42a34a7cc04d
Feng Xiong <fengxiong34@gmail.com>
2020-11-12 10:15:12 -0800

    added loginform

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Login from "./pages/Login";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
 const App: React.FC = () => {
-  return <div>app</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={() => <Login></Login>}></Route>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Form, Input, Button, Checkbox } from "antd";
+
+const StyledForm = styled(Form)`
+  width: 360px;
+  margin-top: 100px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginBtnClick = () => {
+    console.log([email, password]);
+  };
+
+  const handleEmailChange = (e: any) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: any) => {
+    setPassword(e.target.value);
+  };
+
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <StyledForm
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item rules={[{ required: true, message: "Please input your email!" }]}>
+        <Input onChange={handleEmailChange} placeholder="Email:" />
+      </Form.Item>
+
+      <Form.Item
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password onChange={handlePasswordChange} placeholder="Password:" />
+      </Form.Item>
+
+      <Form.Item name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item>
+        <Button
+          style={{ width: 360 }}
+          type="primary"
+          htmlType="submit"
+          onClick={handleLoginBtnClick}
+        >
+          Login
+        </Button>
+      </Form.Item>
+    </StyledForm>
+  );
+}
+
+export default LoginForm;
