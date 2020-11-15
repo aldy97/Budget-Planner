4b875238c7ac3d8fa793ade621ce5686b69214a5
Feng Xiong <fengxiong34@gmail.com>
2020-11-15 15:50:03 -0800

    content is working properly with record addition

--- client/src/components/Overview/Content.tsx
+++ client/src/components/Overview/Content.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SummaryBox from "./SummaryBox";
 import { Layout } from "antd";
-import axios from "axios";
 import { connect } from "react-redux";
 import { RootState } from "../../reducers/index";
 
@@ -14,28 +13,33 @@ export interface Record {
 }
 
 interface ContentProps {
-  user?: string;
+  records?: Record[];
 }
 
-function Content({ user }: ContentProps) {
+function Content({ records }: ContentProps) {
   const { Content } = Layout;
 
-  // const fetchRecordsFromServer = async () => {
-  //   const response = await axios.get(`/api/getRecords/${user}`);
-  //   const records: Record[] = response.data;
-  //   const expense: Record[] = records.filter(record => record.type === "expense");
-  //   const income: Record[] = records.filter(record => record.type === "income");
-  //   let expenseAmount = 0;
-  //   let incomeAmount = 0;
-  //   expense.map(record => (expenseAmount += record.amount));
-  //   income.map(record => (incomeAmount += record.amount));
-  //   console.log(expenseAmount);
-  //   console.log(incomeAmount);
-  // };
+  const [expenseMonthly, setExpenseMonthly] = useState(0);
+  const [incomeMonthly, setIncomeMonthly] = useState(0);
 
-  // useEffect(() => {
-  //   fetchRecordsFromServer();
-  // }, []);
+  const getExpenseAndIncome = () => {
+    let expense = 0;
+    let income = 0;
+    const expenseRecords: Record[] = records
+      ? records.filter(record => record.type === "expense")
+      : [];
+    const incomeRecords: Record[] = records
+      ? records.filter(record => record.type === "income")
+      : [];
+    expenseRecords.map(records => (expense += records.amount));
+    incomeRecords.map(records => (income += records.amount));
+    setExpenseMonthly(expense);
+    setIncomeMonthly(income);
+  };
+
+  useEffect(() => {
+    getExpenseAndIncome();
+  }, [records]);
 
   return (
     <Content style={{ margin: "0 16px" }}>
@@ -43,8 +47,8 @@ function Content({ user }: ContentProps) {
         className="site-layout-background"
         style={{ padding: 24, minHeight: 360, marginTop: 30 }}
       >
-        <SummaryBox type="expense" amount={300}></SummaryBox>
-        <SummaryBox type="income" amount={1000}></SummaryBox>
+        <SummaryBox type="expense" amount={expenseMonthly}></SummaryBox>
+        <SummaryBox type="income" amount={incomeMonthly}></SummaryBox>
       </div>
     </Content>
   );
@@ -52,7 +56,7 @@ function Content({ user }: ContentProps) {
 
 const mapStateToProps = (state: RootState) => {
   return {
-    user: state.HomeReducer.uid,
+    records: state.HomeReducer.records,
   };
 };
 
