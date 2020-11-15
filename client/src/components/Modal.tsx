0cb00300f12626d9c9ba3dc078631d7d94ffcbc5
Feng Xiong <fengxiong34@gmail.com>
2020-11-15 13:25:45 -0800

    can add new records from client to sever

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,17 +1,45 @@
 import React, { useState } from "react";
 import RecordInput from "./RecordInput";
+import axios from "axios";
 import { Modal } from "antd";
+import { connect } from "react-redux";
+import { RootState } from "../reducers/index";
 
 interface ModalProps {
   visible: boolean;
   setVisible: any;
+  user?: string;
+  title?: string;
+  type?: string;
+  category?: string;
+  amount?: number;
+  description?: string;
 }
 
-function AddRecordModal({ visible, setVisible }: ModalProps) {
+function AddRecordModal({
+  visible,
+  setVisible,
+  user,
+  title,
+  type,
+  category,
+  amount,
+  description,
+}: ModalProps) {
   const [confirmLoading, setConfirmLoading] = useState(false);
 
   // TODO: update DB when OK is hit, reupdate Redux
-  const handleOk = () => {
+  const handleOk = async () => {
+    const request = {
+      title,
+      user,
+      type,
+      category,
+      amount,
+      description,
+    };
+    const response = await axios.post("/api/createRecord", request);
+    console.log(response);
     setVisible(false);
   };
 
@@ -34,4 +62,15 @@ function AddRecordModal({ visible, setVisible }: ModalProps) {
   );
 }
 
-export default AddRecordModal;
+const mapState = (state: RootState) => {
+  return {
+    user: state.HomeReducer.uid,
+    title: state.ModalReducer.title,
+    type: state.ModalReducer.recordType,
+    category: state.ModalReducer.category,
+    amount: state.ModalReducer.amount,
+    description: state.ModalReducer.description,
+  };
+};
+
+export default connect(mapState, null)(AddRecordModal);
