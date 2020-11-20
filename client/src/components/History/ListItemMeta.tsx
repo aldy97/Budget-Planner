b7de39738083dffc22633dbd97afa8871277f0b4
Feng Xiong <fengxiong34@gmail.com>
2020-11-20 09:17:52 -0800

    edit modal can be updated:

--- client/src/components/History/ListItemMeta.tsx
+++ client/src/components/History/ListItemMeta.tsx
@@ -1,22 +1,63 @@
 import React from "react";
 import Tag from "../Overview/CategoryTag";
-import { List, Input, Button } from "antd";
+import { List, Input, InputNumber, DatePicker } from "antd";
 import { Record } from "../Overview/Content";
+import moment from "moment";
+import {
+  EDIT_TITLE,
+  EditTitle,
+  EditDescription,
+  EDIT_DESCRIPTION,
+  EditAmount,
+  EDIT_AMOUNT,
+  EDIT_RECORD_DATE,
+  EditRecordDate,
+} from "../../actions/EditModallAction";
 import { connect } from "react-redux";
+import { Dispatch } from "redux";
 import { RootState } from "../../reducers/index";
 
 interface MetaProps {
   item: Record;
   recordID?: string;
+  updateTitle?: any;
+  updateDesc?: any;
+  updateAmount?: any;
+  updateDate?: any;
 }
 
-function ListItemMeta({ item, recordID }: MetaProps) {
+// History内列表内容解构
+function ListItemMeta({
+  item,
+  recordID,
+  updateTitle,
+  updateDesc,
+  updateAmount,
+  updateDate,
+}: MetaProps) {
+  const handleTitleChange = (e: any) => {
+    updateTitle(e.target.value);
+  };
+
+  const handleDescChange = (e: any) => {
+    updateDesc(e.target.value);
+  };
+
+  const handleAmountChange = (value: any) => {
+    updateAmount(value);
+  };
+
+  const handleDateChange = (date: any, dateString: string) => {
+    updateDate(dateString);
+  };
+
   return (
     <>
       <List.Item.Meta
         title={
           recordID === item._id ? (
             <Input
+              onChange={handleTitleChange}
               size="small"
               style={{ width: 100 }}
               defaultValue={item.title}
@@ -30,6 +71,8 @@ function ListItemMeta({ item, recordID }: MetaProps) {
             <Tag type={item.type} category={item.category}></Tag>
             {recordID === item._id ? (
               <Input
+                allowClear
+                onChange={handleDescChange}
                 size="middle"
                 style={{ marginTop: 5 }}
                 defaultValue={item.description}
@@ -42,16 +85,27 @@ function ListItemMeta({ item, recordID }: MetaProps) {
       />
       <div style={{ marginRight: 30, color: "#8c8c8c" }}>
         {recordID === item._id ? (
-          <Input
+          <InputNumber
             size="small"
+            onChange={handleAmountChange}
+            formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
             style={{ width: 80 }}
             defaultValue={item.amount}
-          ></Input>
+          ></InputNumber>
         ) : (
           <div>${item.amount}</div>
         )}
-        <div>{item.recordDate}</div>
-        {/* {recordID === item._id && <Button type="primary">Confirm</Button>} */}
+        <div>
+          {recordID === item._id ? (
+            <DatePicker
+              onChange={handleDateChange}
+              defaultValue={moment(item.recordDate)}
+              size="small"
+            ></DatePicker>
+          ) : (
+            item.recordDate
+          )}
+        </div>
       </div>
     </>
   );
@@ -63,4 +117,37 @@ const mapState = (state: RootState) => {
   };
 };
 
-export default connect(mapState, null)(ListItemMeta);
+const mapDispatch = (dispatch: Dispatch) => {
+  return {
+    updateTitle(title: string) {
+      const action: EditTitle = {
+        type: EDIT_TITLE,
+        title,
+      };
+      dispatch(action);
+    },
+    updateDesc(description: string) {
+      const action: EditDescription = {
+        type: EDIT_DESCRIPTION,
+        description,
+      };
+      dispatch(action);
+    },
+    updateAmount(amount: number) {
+      const action: EditAmount = {
+        type: EDIT_AMOUNT,
+        amount,
+      };
+      dispatch(action);
+    },
+    updateDate(date: string) {
+      const action: EditRecordDate = {
+        type: EDIT_RECORD_DATE,
+        date,
+      };
+      dispatch(action);
+    },
+  };
+};
+
+export default connect(mapState, mapDispatch)(ListItemMeta);
